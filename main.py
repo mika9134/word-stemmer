002f9df67030856f899b08a60146433236ba9a3a
[Mikiyas] <[mikasol9134@gmail.com]>
2024-07-21 09:49:48 -0400

    making the code to read from txt file

--- main.py
+++ main.py
@@ -1,22 +1,57 @@
+# from nltk.stem import SnowballStemmer
+#
+# # Text to be stemmed
+# text = "Ho visto un palazzo rosso"
+#
+# # Create an Italian SnowballStemmer object
+# italian_stemmer = SnowballStemmer("italian")
+#
+# # Split the text into words
+# words = text.split()
+#
+# # Stem each word and store them in a list
+# stemmed_words = []
+# for word in words:
+#   stemmed_words.append(italian_stemmer.stem(word))
+#
+# # Join the stemmed words back into a string
+# stemmed_text = " ".join(stemmed_words)
+#
+# # Print the original and stemmed text
+# print("Original Text:", text)
+# print("Stemmed Text:", stemmed_text)
+
+
 from nltk.stem import SnowballStemmer
 
-# Text to be stemmed
-text = "Ho visto un palazzo rosso"
+# Define the path to your text file
+file_path = "your_text.txt"
 
 # Create an Italian SnowballStemmer object
 italian_stemmer = SnowballStemmer("italian")
 
-# Split the text into words
-words = text.split()
-
-# Stem each word and store them in a list
+# Initialize an empty list to store stemmed words
 stemmed_words = []
-for word in words:
-  stemmed_words.append(italian_stemmer.stem(word))
 
-# Join the stemmed words back into a string
-stemmed_text = " ".join(stemmed_words)
+# Open the text file for reading
+with open(file_path, "r") as text_file:
+  # Read all lines from the file
+  lines = text_file.readlines()
 
-# Print the original and stemmed text
-print("Original Text:", text)
-print("Stemmed Text:", stemmed_text)
+  # Loop through each line and process words
+  for line in lines:
+    # Preprocess the line (optional - remove punctuation etc.)
+    # ... (add preprocessing steps here)
+
+    # Split the line into words
+    words = line.split()
+
+    # Stem each word and add it to the list
+    for word in words:
+      stemmed_word = italian_stemmer.stem(word)
+      stemmed_words.append(stemmed_word)
+
+# Print the stemmed words (optional)
+print(stemmed_words)
+
+# You can further process the stemmed_words list here
